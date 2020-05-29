65db4bc5ae178df8c81ab8b1a137360b83218a52
Gabriel Astudillo <gabriel.astudillo@gmail.com>
2020-05-29 14:59:47 -0400

    Add files via upload

--- bash-examples/clase10/ejemplo.sh
+++ bash-examples/clase10/ejemplo.sh
@@ -1,1 +1,36 @@
+#!/bin/bash
 
+# La variable $# es equiv a argc
+if [ $# != 1 ]; then
+	echo "Uso: $0 <directorio busqueda>"
+	exit
+fi
+
+searchDir=$1
+
+# Falta verificar que searchDir es realmente un directorio
+if [ ! -e $searchDir ]; then
+        echo "Directorio $1 no existe"
+        exit
+fi
+
+printf "Directorio busqueda: %s\n" $1
+ 
+#Idea de Jorge Rodriguez
+
+usePhoneFiles=(`find $searchDir -name '*.txt' -print | sort | grep usePhone | grep -v '._'`)
+
+rm -f fracaso.txt
+for i in ${usePhoneFiles[*]}; 
+do	
+	printf "> %s\n" $i
+	tiempos=(`cat $i | tail -n+3 | cut -d ':' -f 3`)
+	for i in ${tiempos[*]};
+	do
+		printf "%d:" $i >> fracaso.txt
+	done
+	printf "\n" >> fracaso.txt
+	
+done 
+
+ 
